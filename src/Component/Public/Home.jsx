1d2a9f5bf89f6f31325696d87bb8484f909d52e0
ejmahadiair <ejmahadiair@gmail.com>
2022-12-24 07:46:35 +0600

    updated

---
+++ src/Component/Public/Home.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Menu } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+const Home = () => {
+  const navigate = useNavigate();
+  const items = [
+    {
+      label: "Home",
+      key: "home",
+      onClick: () => {
+        navigate("/");
+      },
+    },
+    {
+      label: "Cart",
+      key: "cart",
+      icon: <ShoppingCartOutlined />,
+      onClick: () => {
+        navigate("/cart");
+      },
+    },
+    {
+      label: "Log In",
+      key: "login",
+      onClick: () => {
+        navigate("/login");
+      },
+    },
+    {
+      label: "Register",
+      key: "register",
+      onClick: () => {
+        navigate("/register");
+      },
+    },
+  ];
+  return (
+    <>
+      <Menu
+        onClick={() => {}}
+        // selectedKeys={["home"]}
+        mode="horizontal"
+        items={items}
+      />
+      <Outlet />
+    </>
+  );
+};
+
+export default Home;
